83f2446826d62f22e3a639869ae6b0ee2832e660
Your Name <you@example.com>
2021-11-10 19:20:42 +0700

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "OnGet in Model";
+        }
+        public void OnPost()
+        {
+            Message = "OnPost in Model";
+        }
+    }
+}
+
